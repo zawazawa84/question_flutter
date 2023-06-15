804c8518fcae18e82453633c46dbda1f71cb1f09
aizawa <ikiatawazia@outlook.jp>
2023-06-15 01:13:07 -0700

    mapを用いたボタンの配置

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:question/answer_button.dart';
+import 'package:question/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,6 +14,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
@@ -24,11 +27,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               color: Colors.white,
             ),
           ),
-          const SizedBox(height: 30,),
-          AnswerButton(
-            answerText: 'Answer', 
-            onTap: () {}
-          )
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          })
         ],
       ),
     );
